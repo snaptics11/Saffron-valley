2d12791a1f102470bc652a5e8a84cd54fe887d92
snaptics11 <seo@snaptics.in>
2025-08-21 14:06:42 +0530

    updated vite.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/landing/', 
+  base: './',   // ✅ relative paths work on Netlify
   plugins: [react()],
 })
-
